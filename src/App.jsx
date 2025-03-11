ddaab8707989dcad2fa1b9cf2ed469193117d116
Flavio vMix <flaviovmix@gmail.com>
2025-03-11 17:38:05 -0300

    SETMESSAGE

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("Olá Mundo");
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button
+        onClick={() => {
+          setMessage("Olá, fui clicado");
+        }}
+      >
+        Mudar Mensagem
+      </button>
+    </div>
+  );
+}
+
+export default App;
